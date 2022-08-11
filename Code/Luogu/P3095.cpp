3facf0bc2271ec425e2f05e8c5ea928f81a08af3
rbtree-co <RBTree.net@gmail.com>
2022-08-11 22:05:20 +0800

    Create P3095.cpp

---
+++ Code/Luogu/P3095.cpp
@@ -0,0 +1,180 @@
+// By rbtree (https://rbtree.archi) :\
+// Please submit with C++14!
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <valarray>
+#include <vector>
+#define ra _Read_Int()
+#define rc _Read_Char()
+#define rs _Read_String()
+#define rr _READ_RAW_::_Rd()
+#define iotop _Get_IO_Top()
+#define __FAST__ 1
+#ifdef ___RB_DEBUG___
+#include "rb_debug.h"
+#else
+#define dbg(...)
+#define dputs(...)
+#endif
+
+using tp = long long;
+tp _Read_Int();
+char _Read_Char();
+char _Get_IO_Top();
+std::string _Read_String();
+namespace _READ_RAW_ {
+char _Rd();
+}  // namespace _READ_RAW_
+using namespace std;
+constexpr bool __MTCS__ = 0;
+constexpr tp _BUF_SIZE_ = 217'217'21;
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+namespace __SOL__ {
+void main([[maybe_unused]] size_t __CASE__) {  // :/
+  constexpr tp Hat_LM = 30, Hat_M = 1e5 + 3;
+  tp n = ra, m = ra, q = ra;
+  array<array<tp, Hat_M>, Hat_LM> p;
+  for (tp i = m; i; --i) {
+    p[0][m - ra + 1] = i - 1;
+  }
+  for (tp i = 1; i < Hat_LM; ++i) {
+    for (tp j = 1; j <= m; ++j) {
+      p[i][j] = p[i - 1][p[i - 1][j]];
+    }
+  }
+  while (q--) {
+    tp q = ra, t = q < m ? m : q, l = n - t + 1, ql = 0;
+    q = q < m ? q : m;
+    for (tp i = Hat_LM - 1; ~i; --i) {
+      if (tp loc = ql + (1ll << i); loc <= l && p[i][q]) {
+        ql = loc;
+        q = p[i][q];
+        t += 1ll << i;
+      }
+    }
+    printf("%lld\n", n - t + m - (ql != l ? p[0][q] : q - 1));
+  }
+}  // :)
+}  // namespace __SOL__
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+signed main() {
+  tp __MTCS__ = ::__MTCS__ ? ra : 1;
+  for (tp __i = 1; __i <= __MTCS__; ++__i) {
+    __SOL__::main(__i);
+  }
+  return EXIT_SUCCESS;
+}
+
+namespace _READ_RAW_ {
+#if __FAST__
+std::array<char, _BUF_SIZE_ + 1> _BUF_;
+std::array<char, _BUF_SIZE_>::iterator __Cur = _BUF_.begin(), __End = __Cur + 1;
+
+char _Rd() {
+  if (++__Cur == __End) {
+    __Cur = _BUF_.begin();
+    __End = __Cur + fread(_BUF_.begin(), 1, _BUF_SIZE_, stdin);
+    *__End++ = 10;
+  }
+  return *__Cur;
+}
+}  // namespace _READ_RAW_
+
+char _Get_IO_Top() {
+  if (_READ_RAW_::__Cur + 1 == _READ_RAW_::__End) {
+    _READ_RAW_::__Cur = _READ_RAW_::_BUF_.begin();
+    _READ_RAW_::__End = _READ_RAW_::__Cur +
+                        fread(_READ_RAW_::_BUF_.begin(), 1, _BUF_SIZE_, stdin);
+    if (_READ_RAW_::__Cur == _READ_RAW_::__End) {
+      return --_READ_RAW_::__Cur, -1;
+    } else {
+      return *_READ_RAW_::__Cur--;
+    }
+  }
+  return *(_READ_RAW_::__Cur + 1);
+#else
+char _Rd() {
+  return getchar();
+}
+#endif
+}  // namespace _READ_RAW_
+
+tp _Read_Int() {
+  bool __neg(0);
+  char __c(_READ_RAW_::_Rd());
+  tp __val;
+  for (; __c < 48 || __c > 57; __c = _READ_RAW_::_Rd()) {
+    __neg = __c == 45;
+  }
+  __val = __c & 15;
+  for (__c = _READ_RAW_::_Rd(); __c > 47 && __c < 58; __c = _READ_RAW_::_Rd()) {
+    __val = __val * 10 + (__c & 15);
+  }
+  return __neg ? ~__val + 1 : __val;
+}
+char _Read_Char() {
+  char __c(_READ_RAW_::_Rd());
+  for (; __c == 32 || __c == 10 || __c == 13; __c = _READ_RAW_::_Rd()) {
+  }
+  return __c;
+}
+std::string _Read_String() {
+  char __c(_READ_RAW_::_Rd());
+  std::string __val;
+  for (; __c == 32 || __c == 10 || __c == 13; __c = _READ_RAW_::_Rd()) {
+  }
+  do {
+    __val.push_back(__c);
+    __c = _READ_RAW_::_Rd();
+  } while (__c != 32 && __c != 10 && __c != 13);
+  return __val;
+}
+
+//\
+ ___       ___         ___________     \
+|\  \     |\  \       |\    ___   \
+\ \  \    \ \  \      \ \   \|_\   \
+ \ \  \  __\ \  \      \ \    ___   \
+  \ \  \|\__\_\  \      \ \   \  \   \
+   \ \____________\      \ \___\  \___\
+    \|____________|       \|___|  |___|
+
+/*#################################################################
+#.................................................................#
+#............................This.Code.Was.Created.By.RBTree......#
+#.............#......#...............Limiting-Factor..............#
+#............#.#....#.#.................Soul-Code.................#
+#.............########............................................#
+#............#........#..##############################...........#
+#...........#..V....V......#..#........................#..#...#...#
+#............#........#....#..........###..###..........#..#.#.#..#
+#............#..X##X..#..#............#....#.#...........#..#...#.#
+#...........#...N##N...#..#...........###..###..........#.........#
+#.......MOE..#..@.....#....#.#.#.#...................#.#..........#
+#.............########.....#.#.#.##############.#.#..#.#..........#
+#..........................#.#.#.#.............#.#.#.#.#..........#
+#......#########...........#.#.#.#.................#.#.#..........#
+#.....#.........#..........#.#.#.#.................#.#.#..........#
+#.#.#.#G#R#A#S#S#.#.#......#.#.#.#.................#.#.#..........#
+#.###################......#.#.#.#.................#.#.#..........#
+#...........................#.#.#...................#.#...........#
+#.................................................................#
+#################################################################*/
